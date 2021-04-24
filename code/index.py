4cfefb424c87a846074177ee0ac83b7a68f1acad
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-24 18:05:48 +0800

    fixed only currentqueuenumber returnning

--- code/index.py
+++ code/index.py
@@ -29,33 +29,43 @@ def handler(event, context):
     # Get your queue no and other details
     query = "SELECT q.status, q.queueNumber, q.branchId,b.name as branchName,b.addr as branchAddr,b.postal as branchPostal, c.id as clinicId, c.name as clinicName, q.customerId \
         FROM Queue q,Branch b,Clinic c \
-            WHERE customerId= '{}' and q.branchId=b.id and b.clinicId=c.id and q.status='Q'".format(event['customerId'])  
+            WHERE customerId='{}' and q.branchId=b.id and b.clinicId=c.id and (q.status='Q' or q.status='P' or q.status='D')".format(event['customerId'])  
     cur.execute(query)
     connection.commit()
+    if cur.rowcount > 0:
 ## Construct body of the response object
-    transactionResponse = {}
-    rows = cur.fetchall()
-    branchId = 0
-    for row in rows:
-        print("TEST {0} {1} {2} {3} {4} {5} {6} {7} {8}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]))
-        transactionResponse['status'] = row[0]
-        transactionResponse['yourQueueNumber'] = row[1]
-        transactionResponse['branchId'] = row[2]
-        transactionResponse['branchName'] = row[3]
-        transactionResponse['branchAddr'] = row[4]
-        transactionResponse['branchPostal'] = row[5]
-        transactionResponse['clinicId'] = row[6]
-        transactionResponse['clinicName'] = row[7]
-        transactionResponse['customerId'] = row[8]
-        branchId = row[2]
-    # Get current queue no
-    query="SELECT min(queueNumber) FROM Queue WHERE branchId={} and status='D' or status='P' or status='C'".format(branchId)
-    cur.execute(query)
-    connection.commit()
-    rows = cur.fetchall()
-    for row in rows:
-        print("current queue no: {0}".format(row[0]))
-        transactionResponse['currentQueueNumber']=row[0]
+        transactionResponse = {}
+        rows = cur.fetchall()
+        branchId = 0
+        for row in rows:
+            print("TEST {0} {1} {2} {3} {4} {5} {6} {7} {8}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]))
+            transactionResponse['status'] = row[0]
+            transactionResponse['yourQueueNumber'] = row[1]
+            transactionResponse['branchId'] = row[2]
+            transactionResponse['branchName'] = row[3]
+            transactionResponse['branchAddr'] = row[4]
+            transactionResponse['branchPostal'] = row[5]
+            transactionResponse['clinicId'] = row[6]
+            transactionResponse['clinicName'] = row[7]
+            transactionResponse['customerId'] = row[8]
+            branchId = row[2]
+        # Get current queue no
+        if transactionResponse['status']=='Q':
+            query="SELECT max(queueNumber) FROM Queue WHERE branchId={} and (status='D' or status='P' or status='C')".format(branchId)
+            cur.execute(query)
+            connection.commit()
+            rows = cur.fetchall()
+            for row in rows:
+                print("current doctor queueNumber {0}".format(row[0]))
+                transactionResponse['currentQueueNumber']=row[0]
+        else:
+            query="SELECT max(queueNumber) FROM Queue WHERE branchId={} and (status='C')".format(branchId)
+            cur.execute(query)
+            connection.commit()
+            rows = cur.fetchall()
+            for row in rows:
+                print("current completion queueNumber {0}".format(row[0]))
+                transactionResponse['currentQueueNumber']=row[0]
 
 # Construct http response object
     responseObject = {}
